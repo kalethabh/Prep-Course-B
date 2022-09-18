b98abb3a493aea62856abf65b7cdf8fbe9a35b3c
kalethabh <kalethabh@gmail.com>
2022-09-18 16:52:19 -0500

    RESUELTO

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,32 +8,48 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    let suma = 0;
+    let n = 10;
+    for(i=0; i<=n;i++){
+      suma = suma + i;
+    }
+    return suma;
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  var pares = array.filter(array => array % 2 === 0);
+  return pares
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  var cuadrado = array.map(function(elemento){
+    return elemento**2
+  })
+  return cuadrado;
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  var resultado = array.reduce((acumulador, elemento) => {
+    return acumulador = acumulador + elemento;
+  },0);
+  return resultado
+  
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
-}
-  
-  
+  return num.toString().length;
+} 
   // No modificar nada debajo de esta línea
   // --------------------------------
   
